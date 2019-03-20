bb0133fddddbee6d33ac935b1f6c2a6d568ceb72
SuperZouPan <1512587498@qq.com>
2019-03-20 15:47:32 +0800

    修改了HomeController和应用的启动

--- app1/src/main/java/com/newer/zoupan/app1/App1Application.java
+++ app1/src/main/java/com/newer/zoupan/app1/App1Application.java
@@ -2,7 +2,11 @@ package com.newer.zoupan.app1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * 
+ * @author zoupan
+ * @time 2019年3月20日 下午3:46:34
+ */
 @SpringBootApplication
 public class App1Application {
 
